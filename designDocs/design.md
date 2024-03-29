dadbd7bd0db42552052da041a8a5d5e0e29711c7
SedPC <lilshed3@gmail.com>
2024-03-29 15:57:58 -0700

    error fix on quote formatter; update design docs

--- designDocs/design.md
+++ designDocs/design.md
@@ -1,1 +1,9 @@
 # Design Documents
+
+## Sync script
+
+My sync script exposes a single public method which is called upon runtime.
+
+## Custom commands handler
+
+Accepts routine calls and data from the client class, and then returns data to be posted.
--- src/Client/Client.ts
+++ src/Client/Client.ts
@@ -1,11 +1,17 @@
-import { Client, Events, GatewayIntentBits } from "discord.js";
+import { Client, Events, GatewayIntentBits, InteractionResponse } from "discord.js";
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 import { MyCommands } from "../MyCommands/MyCommands";
+import { create } from "domain";
 
 dotenv.config({ path: path.join(process.cwd(), '../.env') });
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+//create new connxn to the Discord client, from the bots perspective.
+const client = new Client(
+    {
+        intents: [GatewayIntentBits.Guilds] 
+    }
+);
 
 client.once(Events.ClientReady, (readyClient) => {
     console.log("Bot active as " + readyClient.user.tag);
@@ -16,12 +22,8 @@ client.on(Events.InteractionCreate, (interaction) => {
 
     if(interaction.commandName === "quote") {
         const quote = MyCommands.handleCommand(interaction.commandName, interaction.options.get("text")?.value, interaction.options.get("person")?.value, interaction.options.get("context")?.value);
-        interaction.(quote || '').then(
-            res => {
-                res.pin();
-            }
-        );
-
+        
+        interaction.reply(quote || '');
     }
 //    interaction object does not have a value "commandName"
 });
